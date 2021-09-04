62e22e93002968df17ecf3358cf1cc9fae6a1199
Tiago Gomes <tiagogomes187@gmail.com>
2021-09-03 23:58:47 -0300

    Login card styling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,7 +1,32 @@
+import ButtonIcon from 'components/ButtonIcon';
+
+import './styles.css';
+
 const Login = () => {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      <form>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Senha"
+            name="password"
+          />
+        </div>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+      </form>
     </div>
   );
 };
